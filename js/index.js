471cdea7d20b8769771a35dea782fd81c696a675
Erwan Dehlouz <dehlouz.erwan@gmail.com>
2021-09-08 19:23:36 +0800

    build v.0.1

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(".proj1 img").hover(
+  function() {
+    $(".proj1").toggleClass("hover");
+  }
+);
+
+$(".proj2 img").hover(
+  function() {
+    $(".proj2").toggleClass("hover");
+  }
+);
+
+$(".proj3 img").hover(
+  function() {
+    $(".proj3").toggleClass("hover");
+  }
+);
+
+$(".proj4 img").hover(
+  function() {
+    $(".proj4").toggleClass("hover");
+  }
+);
