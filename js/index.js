6df73ad8817932262ce7e8aaa2b1305cc97b9e4b
Erwan Dehlouz <dehlouz.erwan@gmail.com>
2021-09-20 11:41:40 +0800

    build v.0.2

--- js/index.js
+++ js/index.js
@@ -21,3 +21,18 @@ $(".proj4 img").hover(
     $(".proj4").toggleClass("hover");
   }
 );
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
