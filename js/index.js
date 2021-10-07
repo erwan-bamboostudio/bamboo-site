7514a74cadb6a8d5e77c01e75a65a5149d838776
Erwan Dehlouz <erwan@bamboocreativestudio.com>
2021-10-07 17:26:18 +0800

    build v.0.6

--- js/index.js
+++ js/index.js
@@ -14,6 +14,30 @@ $(".proj4 img").hover(function () {
   $(".proj4").toggleClass("hover")
 })
 
+$(".project1 img").hover(function () {
+  $(".project1").toggleClass("hover-work")
+})
+
+$(".project2 img").hover(function () {
+  $(".project2").toggleClass("hover-work")
+})
+
+$(".project3 img").hover(function () {
+  $(".project3").toggleClass("hover-work")
+})
+
+$(".project4 img").hover(function () {
+  $(".project4").toggleClass("hover-work")
+})
+
+$(".project5 img").hover(function () {
+  $(".project5").toggleClass("hover-work")
+})
+
+$(".project6 img").hover(function () {
+  $(".project6").toggleClass("hover-work")
+})
+
 $(".nav-contact a").on("click", function (e) {
   $(".contact").toggleClass("visible")
   $("body").toggleClass("overflow-0")
@@ -40,3 +64,28 @@ for (i = 0; i < acc.length; i++) {
     }
   })
 }
+
+$("button.btn1").on("click", function (e) {
+  $(".panel1").toggleClass("fit-content pad-2")
+  e.preventDefault()
+})
+
+$("button.btn2").on("click", function (e) {
+  $(".panel2").toggleClass("fit-content pad-2")
+  e.preventDefault()
+})
+
+$("button.btn3").on("click", function (e) {
+  $(".panel3").toggleClass("fit-content pad-2")
+  e.preventDefault()
+})
+
+$("button.btn4").on("click", function (e) {
+  $(".panel4").toggleClass("fit-content pad-2")
+  e.preventDefault()
+})
+
+$("button.btn5").on("click", function (e) {
+  $(".panel5").toggleClass("fit-content pad-2")
+  e.preventDefault()
+})
