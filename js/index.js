44363468c30b99aa915e83a8a09c2153693de086
Erwan Dehlouz <erwan@bamboocreativestudio.com>
2021-10-07 12:36:05 +0800

    build v.0.5

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
+$(".proj1 img").hover(function () {
+  $(".proj1").toggleClass("hover")
+})
 
+$(".proj2 img").hover(function () {
+  $(".proj2").toggleClass("hover")
+})
 
-$(".proj1 img").hover(
-  function() {
-    $(".proj1").toggleClass("hover");
-  }
-);
+$(".proj3 img").hover(function () {
+  $(".proj3").toggleClass("hover")
+})
 
-$(".proj2 img").hover(
-  function() {
-    $(".proj2").toggleClass("hover");
-  }
-);
+$(".proj4 img").hover(function () {
+  $(".proj4").toggleClass("hover")
+})
 
-$(".proj3 img").hover(
-  function() {
-    $(".proj3").toggleClass("hover");
-  }
-);
+$(".nav-contact a").on("click", function (e) {
+  $(".contact").toggleClass("visible")
+  $("body").toggleClass("overflow-0")
+  e.preventDefault()
+})
 
-$(".proj4 img").hover(
-  function() {
-    $(".proj4").toggleClass("hover");
-  }
-);
+$(".close-icon").on("click", function (e) {
+  $(".contact").toggleClass("visible")
+  $("body").toggleClass("overflow-0")
+  e.preventDefault()
+})
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName("accordion")
+var i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active")
+    var panel = this.nextElementSibling
     if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+      panel.style.maxHeight = null
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px"
     }
-  });
+  })
 }
-
